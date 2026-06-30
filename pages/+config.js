c561dd053ba0bb58fe2c951208c3de9255b1ed34
TechNext Marketing <hello@technext.asia>
2026-06-30 21:48:30 +0800

    Convert site from React/Vike SSR to plain static HTML

--- pages/+config.js
+++
@@ -1,9 +0,0 @@
-import vikeReact from 'vike-react/config'
-import vikePhoton from 'vike-photon/config'
-
-export default {
-  extends: [vikeReact, vikePhoton],
-  title: 'Tech Next Solutions - Top IT Services Company in Southeast Asia',
-  description: 'Your Reliable Technology Partner in Southeast Asia. We deliver cutting-edge web development, mobile apps, UI/UX design, and AI solutions.',
-  lang: 'en',
-}
